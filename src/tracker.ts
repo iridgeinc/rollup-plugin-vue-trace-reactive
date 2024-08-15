d55018f31638b97b5d3cb9c397de12661b950298
Motohiro Ueki <motohiro.ueki@iridge.jp>
2024-08-15 17:29:48 +0900

    use CustomEvent.

--- src/tracker.ts
+++ src/tracker.ts
@@ -1,29 +1,26 @@
 import type { DebuggerEvent } from 'vue'
+import type { TracedReactiveTriggerEvent } from '../client'
 
 function triggerPlace(e: Error) {
-  // console.log(e)
-  const regex = /ReactiveEffect\.onTrigger.*?\((?<url>.*?:(?<line>\d+?):(?<column>\d+?))\)/
+  const regex = /\.onTrigger.*?\((?<url>.*?:(?<line>\d+?):(?<column>\d+?))\)/
   const match = e.stack!.match(regex)
   const url = new URL(match?.groups?.url ?? "/unknown")
   return {
-    filepath: url.pathname.split('/').pop(),
-    line: match?.groups?.line ?? 0,
-    column: match?.groups?.col ?? 0,
+    filepath: url.pathname,
+    line: Number(match?.groups?.line ?? 0),
+    column: Number(match?.groups?.col ?? 0),
   }
 }
 
 export const handleReactiveTrigger = (type: string, varname: string, e: DebuggerEvent, ee: Error) => {
   const { filepath, line } = triggerPlace(ee)
-  const place = filepath + ':' + line
-  const callee = type + (varname !== undefined ? "." + varname : "")
-  const action = e.key + "." + e.type
-  const message = (() => {
-    if (e.oldValue !== undefined && e.newValue !== undefined) {
-      const isSame = JSON.stringify(e.oldValue) === JSON.stringify(e.newValue)
-      return isSame ? "ok" : "same value!"
-    } else {
-      return '-'
-    }
-  })();
-  console.log(`${place}\t${callee}\t${action}\tsame:${message}`)
+  const ev: TracedReactiveTriggerEvent = {
+    event: e,
+    filepath,
+    line,
+    watcherType: type,
+    watcherBind: varname,
+  }
+  const event = new CustomEvent('trace-reactive-trigger', { detail: ev })
+  document.dispatchEvent(event)
 }
